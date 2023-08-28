e0b5425038b7ba8810632c6ee2debd2c08e8fcce
Sadgman <angel.luis.regalado@hotmail.com>
2023-08-28 02:37:48 -0400

    I added some code to the JavaScript file and changed styles.

--- fS.js
+++ fS.js
@@ -1,10 +1,16 @@
-l = $("#img-animep").attr("src")
+// Wait for the page to load
+document.addEventListener("DOMContentLoaded", function(){
+    const title = document.getElementById("fan") // I get the content of the h2 element
+    if(window.innerWidth <= 767){ // Check the size of the window.
+        title.textContent = "AFS"
+    }
 
-$("#flechaR").click(function(){
-    $("#flechaL").show()
-    $("#img-animep").attr("src","font.jpeg")
-})
-$("#flechaL").click(function(){
-    $("#img-animep").attr("src", l)
-    $(this).hide()
+    window.addEventListener('resize', function() {
+        if (window.innerWidth <= 767) {
+            title.textContent = "AFS"
+        }
+        else{
+            title.textContent = "ANIME FAN SITE"
+        }
+    });
 })
